75406e62cc0f875076ceb7e5bf634a657cf22c9e
toebay007 <toebay007@gmail.com>
2022-08-06 10:44:42 +0100

    Update verify.php

--- verify.php
+++ verify.php
@@ -7,8 +7,74 @@
             </ol>
         </nav>
         <br><br>
+        <div class="cards mb-4">
+            <form action="#" method="post">
+                <h3 class="text-center mt">Verification</h3>
 
-        
+                <div class="row g-2">
+                    <div class="col-md">
+                        <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInputGrid11" placeholder="Firstname">
+                        <label for="floatingInputGrid11">Enter Staff Unique Number</label>
+                        <div id="emailHelp" class="form-text"><i>Search by Unique ID</i></div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        
+                    </div>
+                    <div class="col-md">
+                        <div class="form-floating">
+                        <input type="submit" class="form-control" id="floatingInputGrid" placeholder="Search">
+                       
+                        </div>
+                    </div>
+                </div>
+                <hr width="80%" style="color: grey;">
+            </form>
+            <form class="mt-2" action="#" method="post">
+            <div class="row g-2">
+                    <div class="col-md">
+                        <h4>Search by fingerprint</h4>
+                    </div>
+                    <div class="col-md">
+                        
+                    </div>
+                    <div class="col-md">
+                        <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInputGrid11" placeholder="Firstname">
+                        <label for="floatingInputGrid11">Click to capture fingerprint</label>
+                        <div id="emailHelp" class="form-text"><i>Search by Staff fingerprint</i></div>
+                        </div>
+                    </div>
+                </div>
+            <hr width="80%" style="color: grey;">
+            </form>
+            
+            <form action="#" method="post">
+                <div class="row g-2">
+                    <div class="col-md">
+                        <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInputGrid11" placeholder="Firstname">
+                        <label for="floatingInputGrid11">Enter Staff Name</label>
+                        <div id="emailHelp" class="form-text"><i>Search profile with staff name</i></div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        
+                    </div>
+                    <div class="col-md">
+                        <div class="form-floating">
+                        <input type="submit" class="form-control" id="floatingInputGrid" placeholder="Search">
+                       
+                        </div>
+                    </div>
+                </div>
+                <hr width="80%" style="color: grey;">
+            </form>
+
+        </div>
+
+        <div class="bodis"></div>
     </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
